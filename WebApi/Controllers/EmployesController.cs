20cbd711fe8ec96e6bfa4f436e52e93ea04161a3
Miraç Ozan <Mira Ozan@MIRA>
2022-08-08 17:19:51 +0300

    WebApi İmplements

---
+++ WebApi/Controllers/EmployesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[Employess]")]
+    [ApiController]
+    public class EmployesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
